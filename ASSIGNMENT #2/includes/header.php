f4795d53f987318d7cfc5a464e83c9b4858ae7d0
Navjotsingh13a.github.io <navjotsingh57@dcmail.ca>
2024-12-18 18:48:20 -0500

    ASSIGNMENT #2

---
+++ ASSIGNMENT #2/includes/header.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INFT2100 - <?php echo $title; ?></title>
+     <!--
+        Name: Navjot Singh
+        File: index.php
+        Date: November 10, 2024
+        Description: Home page for INFT2100 Website
+        -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+    <header class="bg-danger text-black text-center p-3">
+        <h1><?php echo $banner; ?></h1>
+    </header>
---
+++ ASSIGNMENT #2/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Set page title, file, description, date, and banner
+$title = "Home Page";
+$file = "index.php";
+$banner = "Students Grade Portal";
+$description = "Home page for my INFT2100 Website";
+$date = "November 10, 2024";
+
+
+// Include the header file
+include('./includes/header.php');
+?>
+
+<!-- Display welcome message and description of the portal -->
+<h1>Welcome to the Students Grade Portal</h1>
+<p>This website allows students to check their grades and information.</p>
+
+<?php 
+// Include the footer file
+include('./includes/footer.php'); 
+?>
